aa672556e43d171e76c6b8f7a996779856db8c84
Rani Abu Raia <raniaburaia@Ranis-MacBook-Air-3.local>
2023-07-02 16:31:38 +0300

    Node.js tutorial proj

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express (); 
+const morgan = require('morgan');
+const bodyParser = require ('body-parser');
+
+
+
+// handels routes request
+const productRoutes=require('./api/routes/products')
+const ordersRoutes=require('./api/routes/orders')
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next )=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With, Content-Type,Accept,Authorization"
+    );
+    if(req.method=='OPTIONS'){
+        res.header('Acess-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use('/products',productRoutes)
+app.use('/orders',ordersRoutes)
+
+app.use((req,res,next)=> {
+    const error = new Error ('Not Found');
+    error.status=404;
+    next(error);
+})
+
+app.use((error,req,res,next)=> {
+    res.status(error.status|| 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    });
+});
+
+
+module.exports=app;
